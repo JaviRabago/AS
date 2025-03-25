d146bd932b1370b8126b1b9407369f0c07dd364d
javi <javi.rabamonte@alum.uca.es>
2025-03-25 19:32:33 +0100

    añadido ssh a nginx

--- nginx/nginx-wrapper.sh
+++ nginx/nginx-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Script para configurar la ruta predeterminada y luego ejecutar Nginx
+# Script para configurar la ruta predeterminada, iniciar SSH y ejecutar Nginx
 
 # Verificar si la herramienta IP está disponible
 if ! command -v ip > /dev/null 2>&1; then
@@ -11,6 +11,13 @@ fi
 echo "Configurando ruta predeterminada via 172.30.0.2..."
 ip route del default 2>/dev/null || true
 ip route add default via 172.30.0.2
+
+# Generar claves SSH si no existen
+ssh-keygen -A
+
+# Iniciar el servicio SSH
+echo "Iniciando servicio SSH..."
+/usr/sbin/sshd
 
 # Ejecutar Nginx
 echo "Iniciando Nginx..."
