b118ab1cc16ed8bd7bbe19fb939085247e09f246
javi <javi.rabamonte@alum.uca.es>
2025-04-01 23:18:47 +0200

    arreglos

--- apache/apache-wrapper.sh
+++ apache/apache-wrapper.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-# Script para configurar la ruta predeterminada
-
-# Verificar si la herramienta IP está disponible
-if ! command -v ip > /dev/null 2>&1; then
-    echo "Instalando iproute2..."
-    apk add --no-cache iproute2
-fi
 
 # Configurar la ruta predeterminada para la red de desarrollo
 echo "Configurando ruta predeterminada via 172.40.0.150..."
--- router/entrypoint-router.sh
+++ router/entrypoint-router.sh
@@ -50,10 +50,10 @@ iptables -A FORWARD -s $DEV_NET -d $SERVICE_NET -j ACCEPT
 # 3. CONFIGURACIÓN DE POSTGRES (EXCEPCIÓN)
 echo "Configurando excepción para postgres..."
 POSTGRES_IP=$(getent hosts prod-postgres | awk '{ print $1 }')
-if [ -z "$POSTGRES_IP" ]; then
-    echo "No se pudo obtener la IP de postgres, usando valor estático"
-    POSTGRES_IP="172.30.0.2"  # IP estática según logs
-fi
+# if [ -z "$POSTGRES_IP" ]; then
+#     echo "No se pudo obtener la IP de postgres, usando valor estático"
+#     POSTGRES_IP="172.30.0.2" 
+# fi
 echo "IP de postgres detectada: $POSTGRES_IP"
 
 # Permitir comunicación entre postgres y DNS
@@ -61,8 +61,7 @@ echo "Permitiendo comunicación específica postgres -> dns..."
 iptables -A FORWARD -s $POSTGRES_IP -d $DNS_IP -j ACCEPT
 iptables -A FORWARD -d $POSTGRES_IP -s $DNS_IP -j ACCEPT
 
-# --- RESOLUCIÓN DNS (limitada) ---
-echo "Configurando acceso limitado al DNS..."
+echo "Configurando acceso al DNS..."
 # Permitir que todos se comuniquen con el DNS
 for net in "$PROD_NET" "$DEV_NET" "$SERVICE_NET" "$VPN_NET"; do
     iptables -A FORWARD -s $net -d $DNS_IP -p udp --dport 53 -j ACCEPT
@@ -132,7 +131,7 @@ echo "Configurando OpenVPN (puerto 1194/UDP)..."
 OPENVPN_IP=$(getent hosts openvpn | awk '{ print $1 }')
 if [ -z "$OPENVPN_IP" ]; then
     echo "No se pudo obtener la IP de OpenVPN, usando valor estático"
-    OPENVPN_IP="172.10.0.3"  # IP estática según logs
+    OPENVPN_IP="172.10.0.3" 
 fi
 echo "IP de OpenVPN detectada: $OPENVPN_IP"
 
@@ -167,7 +166,7 @@ done
 
 # --- BLOQUEO DE TRÁFICO SALIENTE A INTERNET ---
 echo "Aplicando bloqueo adicional para acceso a Internet..."
-# Bloquear todo el tráfico que sale al mundo exterior
+# Bloquear todo el tráfico que sale al exterior
 for net in "$PROD_NET" "$DEV_NET" "$SERVICE_NET" "$VPN_NET"; do
     iptables -A FORWARD -s $net ! -d 10.0.0.0/8 ! -d 172.16.0.0/12 ! -d 192.168.0.0/16 -j DROP
 done
